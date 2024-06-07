c93cff1eda293673c4b45543e148576a4f8fa479
matias-gdrx <matias.zilli@partners.goodrx.com>
2024-06-07 11:58:23 +0200

    chore: add files

---
+++ accounts/dev/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.0.0"
+
+  bucket = "my-s3-bucket-test-tf-matias"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "rds" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
+  version = "6.2.0"
+
+  name = "myrdsdb"
+}
